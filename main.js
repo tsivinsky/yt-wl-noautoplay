f6278a8d1aeb715c73f74208be00ce91e44c5325
Daniil Tsivinsky <daniil@tsivinsky.com>
2025-04-22 15:57:55 +0300

    redirect to original url if no video id in it

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
-const params = new URLSearchParams(location.search);
-const u = params.get("url");
-if (u) {
+(() => {
+  const params = new URLSearchParams(location.search);
+  const u = params.get("url");
+  if (!u) {
+    return;
+  }
+
   const url = new URL(u);
   const urlParams = new URLSearchParams(url.search);
   const videoId = urlParams.get("v");
-  location.href = `https://youtube.com/watch?v=${videoId}`;
-}
+
+  if (videoId) {
+    location.href = `https://youtube.com/watch?v=${videoId}`;
+  } else {
+    location.href = url.toString();
+  }
+})();
